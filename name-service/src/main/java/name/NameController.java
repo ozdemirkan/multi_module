f032a25c2873a726918b1f2665d6b24746fe8faa
Melih Özdemirkan <melih.ozdemirkan@luxonpayments.com>
2019-07-18 11:01:53 +0100

    sonar conf circleci

--- name-service/src/main/java/name/NameController.java
+++ name-service/src/main/java/name/NameController.java
@@ -1,12 +1,13 @@
 package name;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class NameController {
 
-  @RequestMapping("/")
+  @GetMapping("/")
   public String getName() {
     return "Jib Multimodule";
   }
